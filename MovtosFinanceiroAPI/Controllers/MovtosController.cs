50f7b36be640a2cf136adc5082b68baa4f7baf27
rafael sanchez port <rafysanchez@hotmail.com>
2020-02-24 18:53:39 -0300

    ajustres

---
+++ MovtosFinanceiroAPI/Controllers/MovtosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovtosFinanceiroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovtosController : ControllerBase
+    {
+        // GET: api/Movtos
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Movtos/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Movtos
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Movtos/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
